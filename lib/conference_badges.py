cd404d24a0ea36ddb79ebb741f7bb5608b785763
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-01-19 23:15:52 +0300

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    badge = f"Hello, my name is {name}."
+    return badge
 
 def batch_badge_creator(names):
-    return None
+    batch = []
+    for name in names:
+        batch.append(badge_maker(name))
+    return batch
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    index = 0
+    for name in names:
+        assign_room = f"Hello, {name}! You'll be assigned to room {index+1}!"
+        rooms.append(assign_room)
+        index += 1
+    return rooms
 
 def printer(names):
-    return None
+    for name in names:
+        print(badge_maker(name))
+        rooms = assign_rooms(names)
+    for room in rooms:
+        print(room)
